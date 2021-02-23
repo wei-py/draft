a47f6881ae9570ca25119cfd3ee10f0ce1cb2073
wei-py <brook0825@outlook.com>
2021-02-23 22:09:46 +0800

    最终版

--- bilibili_anime.py
+++ bilibili_anime.py
@@ -8,30 +8,34 @@ class bilibili_anime():
         self.lua = '''
                     function main(splash)
                         splash:go("%s")
-                        splash:wait(0.5)
+                        submit = splash:select('#pgc-navigate-wrap > ul > li:nth-child(10) > div')
+                        submit:mouse_click()
+                        splash:wait(3)
                         return splash.html()
                     end
                     '''
 
     # splash 动态加载
-    def source(self, script):
+    def source(self, url):
+        fresh_lua = self.lua
+        fresh_lua = format(fresh_lua % url)
         splash_url = 'http://localhost:8050/execute'
         headers = {'content-type':'application/json'}
-        data = json.dumps({'lua_source': script})
+        data = json.dumps({'lua_source': fresh_lua})
         resp = requests.post(splash_url, headers=headers, data=data)
         return resp
 
+    # 搜索番名
     def search_anime(self):
         anime = input('输入番名:')
         # anime = '犬夜叉'
         url = 'https://search.bilibili.com/all?keyword=%s' % anime
-        fresh_lua = self.lua
-        fresh_lua = format(fresh_lua % url)
-        resp = self.source(fresh_lua)
+        resp = self.source(url) # splash动态加载后return response
 
-        # 查询搜索的番名
+        # 查询搜索的番名，待会检验是否一致
         info_list = re.findall('bangumi-item-wrap(.*?)user-count', resp.text, re.S)
 
+        # 检验番名的正确，推荐其他类似的番（季）
         if len(info_list) == 0:
             print('请输入正确的番名')
             self.search_anime()
@@ -49,40 +53,34 @@ class bilibili_anime():
             release_time = re.findall('开播时间：.*?"value">(.*?)</span>', info_list[0])[0]
             voice_actor = re.findall('声优：.*?title="(.*?)" class="value', info_list[0], re.S)[0]
             introduction = re.findall('<div class="desc">(.*?)</div></div>', info_list[0], re.S)[0].replace('/n', '').replace(' ', '')
-            play_url_list = re.findall('ep-sub.*?href="(.*?)\?from=search', info_list[0], re.S)
-
-
+            play_url_list = re.findall('ep-sub.*?href="(.*?)" target', info_list[0], re.S)
+            
             # print(style, region, release_time, voice_actor, introduction, '总共' + len(play_url_list) + '集')
-            ep_start = int(play_url_list[0].split('/')[-1].replace('ep', ''))
-            ep_end = int(play_url_list[-1].split('/')[-1].replace('ep', ''))
 
-            print('需要下载的集数的范围（一集则输入相同数字），全集共为%d' % (ep_end-ep_start+1))
+            # 操作下载
+            print('共有%d集' + len(play_url_list))
             down_start = int(input('start：'))
             down_end = int(input('end：'))
-
-            if down_end <= (ep_end-ep_start+1):
-                down_start = ep_start + down_start - 1
-                down_end = ep_start + down_end - 1
-                self.Download_Anime(down_start, down_end)
+            if down_end <= len(play_url_list):
+                
+                self.Download_Anime(play_url_list[down_start - 1 : down_end])
             else:
                 print('你输入有误，可能没有第%s集' % down_end)
 
 
-
-    def Download_Anime(self, down_start, down_end):
-        p = down_start
+    # 下载视频
+    def Download_Anime(self, play_url_list):
         flv_queue = Queue()
-        url = 'https://www.bilibili.com/bangumi/play/ep%s'
-        fresh_url = 'https://www.bilibili.com/bangumi/play/ep%s'
-        while p <= down_end:
-            url = fresh_url
-            flv = format(url % str(p))
-            # os.system('you-get --format=flv %s' % flv)
-            flv_queue.put(flv)
-            p += 1
-        while not flv_queue.empty():
-            print(flv_queue.get())
+        for url in play_url_list:
+            flv_queue.put(url)
         
+        while not flv_queue.empty():
+            down_flv = flv_queue.get()
+            print(down_flv)
+        #     os.system('you-get --format=flv360 %s' % down_flv) # 前提是要 pip3 install you-get
+        print('下载完毕')
+
+   
 
 
 if __name__ == '__main__':
